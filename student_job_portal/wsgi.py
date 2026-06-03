a30554dabc4e38a79bfef290a78fdea526bad85f
pknandam29 <pradeepnandam29@gmail.com>
2026-06-03 15:58:06 +0530

    Fix python path for Vercel serverless function in wsgi.py

--- student_job_portal/wsgi.py
+++ student_job_portal/wsgi.py
@@ -1,13 +1,8 @@
-"""
-WSGI config for student_job_portal project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add the project root directory to the python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
